6cdb6c8da147e4037ff8eb2c7d8e1860f3474f3d
Lee In Seok <dlstjr9068@gmail.com>
2020-08-21 20:20:31 +0900

    김현경 피드백 완료

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,6 +5,7 @@ $(function () {
 const indexInit = () => {
   $("#btnYes").click(function () {
     let numberArray = [];
+    $("#numberDiv").empty();
 
     for (let i = 1; i <= 7; i++) {
       let number = randomLotto();
@@ -16,7 +17,23 @@ const indexInit = () => {
       numberArray.push(number);
     }
 
-    console.log(numberArray);
+    numberArray.forEach(function (number) {
+      numberHtml = `<p class="numberClass"><font>${number}</p>`;
+
+      $("#numberDiv").append(numberHtml);
+    });
+
+    // p#number1.numberClass 1
+    // p#number2.numberClass 2
+    // p#number3.numberClass 3
+    // p#number4.numberClass 4
+    // p#number5.numberClass 5
+    // p#number6.numberClass 6
+    // p#number7.numberClass 7
+
+    $(".tube").fadeOut("slow");
+    $("#numberDiv").css("margin-top", "0px");
+    $("#numberDiv").fadeIn(5000);
   });
 };
 
